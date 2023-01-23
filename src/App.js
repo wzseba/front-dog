321a6ebc8a18e7713c182191b5887f74db5bbbed
wzseba <wzseba@gmail.com>
2023-01-23 18:14:00 -0300

    se agrega router dom con defaults axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route} from 'react-router-dom';
+import Landing from './components/Landing/Landing';
 import './App.css';
+import axios from 'axios';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
+
+axios.defaults.baseURL = 'http://localhost:3001/';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/"><Landing/></Route>
+        <Route path="/*"><PageNotFound/></Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
