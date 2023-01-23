00c35d9bd3b6170ae4c8788cb8b0f2ae95e61af1
wzseba <wzseba@gmail.com>
2023-01-23 18:14:41 -0300

    se agragaron componentes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Card = () => {
+const Card = () => {
   return (
     <div>Card</div>
   )
 }
+
+export default Card
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import image from '../../image/dogi.gif';
+
+const Loading = () => {
+  return (
+    <div>
+      <img src={image} alt="cargando..." />
+    </div>
+  )
+}
+
+export default Loading
