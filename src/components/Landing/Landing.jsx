b008f0b0fb677b72e1cd9a28087df68d58386485
wzseba <wzseba@gmail.com>
2023-01-24 00:03:33 -0300

    firts commit

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { getAllDogs } from '../../redux/action';
+
+
 
 const Landing = () => {
+  const allDogs = useSelector(state => state.dogs);
+  // console.log(allDogs);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    !allDogs.length && dispatch(getAllDogs());
+    // !allTemp.length && dispatch(getTemperaments());
+    // eslint-disable-next-line
+  },[])
+
   return (
     <div>Landing</div>
   )
