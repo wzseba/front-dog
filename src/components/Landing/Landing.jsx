008e923dd9bdb3ffd698d21dd7f2a3dce722fb46
wzseba <wzseba@gmail.com>
2023-01-24 17:23:49 -0300

    se agraron iconos de redes sociales

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,24 +1,64 @@
-import React from 'react'
-import { useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import { getAllDogs } from '../../redux/action';
-
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import s from './Landing.module.css';
 
 
 const Landing = () => {
-  const allDogs = useSelector(state => state.dogs);
-  // console.log(allDogs);
-  const dispatch = useDispatch();
-
-  useEffect(()=>{
-    !allDogs.length && dispatch(getAllDogs());
-    // !allTemp.length && dispatch(getTemperaments());
-    // eslint-disable-next-line
-  },[])
+  
 
   return (
-    <div>Landing</div>
+    
+      <div className={s.container}>
+        <div className={s.iz}>
+          <h2>Pi-Dog</h2>
+        </div>
+
+        <div className={s.de}>
+          <div>
+            <h1>DogLandia</h1>
+            <div className={s.link}>
+              <Link  to='/home'>
+                <button className={s.btn}>Ingresar</button>
+              </Link>
+            </div>
+            
+          </div>
+
+          <div className={s.sociales}>
+            <ul>
+                <li >
+                  <a href="https://www.linkedin.com/in/dev-sebastian-vera" target={'_blank'} rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedinIn} size='2xl'/>
+                   
+                  </a>
+                </li>
+
+                <li>
+                  <a href="https://www.github.com/wzseba" target={'_blank'} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} size='2xl'/>
+                   
+                  </a>
+                </li>
+
+                <li >
+                  <a href="mailto:wzseba@gmail.com" target={'_blank'} rel="noreferrer">
+                    <FontAwesomeIcon icon={faEnvelope} size='2xl'/>
+                    
+                  </a>
+                </li>
+              </ul>
+
+              <div className={s.autor}>Designed and built by <span>Sebastian Vera</span></div>
+          </div>
+
+        </div>
+
+      </div>
+    
+    
   )
 }
 
-export default Landing
+export default Landing;
