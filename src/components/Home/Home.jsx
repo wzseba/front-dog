4814ed767e88de22f66c6814dcec9399b0af7836
wzseba <wzseba@gmail.com>
2023-01-28 03:04:37 -0300

    se diseño estructura de navegacion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import s from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <nav className={s.nav_container}>
+        <div className={s.logo}>logo</div>
+        <div>
+          <input type="text" placeholder='Search' />
+          Search
+        </div>
+        <div>
+          <Link to='/formAdd'>
+            crear raza
+          </Link>
+          <Link to='/favorite'>
+            favoritos
+          </Link>
+        </div>
+      </nav>
+    </div>
   )
 }
 
