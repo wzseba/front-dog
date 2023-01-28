f8a3ec65acf224f7b063058fc71054fdb3ddc23f
wzseba <wzseba@gmail.com>
2023-01-28 03:05:17 -0300

    se agrego ruta home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/Landing/Landing';
 
 import axios from 'axios';
 import PageNotFound from './components/PageNotFound/PageNotFound';
+import Home from './components/Home/Home';
 
 
 axios.defaults.baseURL = 'http://localhost:3001/';
@@ -15,6 +16,7 @@ function App() {
     <Router>
       <Switch>
       <Route exact path="/" component={Landing} />
+      <Route exact path="/home" component={Home} />
       <Route path="/*" component={PageNotFound} /> 
       </Switch>
     </Router>
