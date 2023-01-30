61dac23b0780ac6140f9f978a4c715dace8f2d92
wzseba <wzseba@gmail.com>
2023-01-30 01:07:11 -0300

    se hace estructura card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Card = () => {
+import s from './Card.module.css';
+
+
+const Card = ({name, id, image, temperament, weight}) => {
+ 
+
   return (
-    <div>Card</div>
+    <div className={s.card}>
+        <Link to={`/dogDetail/${id}`}>
+
+          <img className={s.img_dog} src={image} alt="img not found" />
+
+            <h3 className={s.card_h3}>{name}</h3>
+            {/* <p>{life_span}</p> */}
+            <p className={s.card_parrafo}>{temperament}</p>
+            <p className={s.card_parrafo}>Weight: Min {weight} Max</p>
+          
+       </Link>
+      </div>
   )
 }
 
