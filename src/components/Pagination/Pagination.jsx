3f0a5da50a10fb2ee70758db301089ec90d3f593
wzseba <wzseba@gmail.com>
2023-01-31 01:05:58 -0300

    se finalizo componente

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
-// import { useState } from 'react';
+import s from './Pagination.module.css';
 
 
 const Pagination = ({dogsPerPage, totalDogs, currentPage, setCurrentPage}) => {
 
   const pageNumbers = [];
+  const cardPerPage = Math.ceil(totalDogs / dogsPerPage);
   
-  for(let i = 1; i <= Math.ceil(totalDogs / dogsPerPage); i++){
+  for(let i = 1; i <= cardPerPage; i++){
     pageNumbers.push(i);
   }
 
+  const onPreviusPage = ()=>{
+    setCurrentPage(currentPage - 1);
+  }
+  const onNextPage = ()=>{
+    setCurrentPage(currentPage + 1);
+  }
+
+  const onSpecificPage = (n)=>{
+    setCurrentPage(n)
+  }
 
   return (
-    <div>Pagination</div>
+    <nav className={s.container_nav}>
+      <button className={s.btn} disabled={ currentPage === 1 ? true : false} onClick={ onPreviusPage }>Anterior</button>
+      
+      <ul className={s.container_ul}>
+        {
+          pageNumbers?.map(page => (
+            
+            <button className={s.btn_pages} onClick={()=>onSpecificPage(page)}>{page}</button>
+           
+          ))
+        }
+      </ul>
+
+      <button className={s.btn} disabled={ currentPage >= pageNumbers.length ? true : false} onClick={ onNextPage }>Siguiente</button>
+    </nav>
   )
 }
 
