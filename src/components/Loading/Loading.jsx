439267bfc663e7a7866dee7e51871c209805a186
wzseba <wzseba@gmail.com>
2023-01-31 18:13:13 -0300

    se armo estructura

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import image from '../../image/dogi.gif';
+import s from './Loading.module.css';
 
 const Loading = () => {
   return (
-    <div>
-      <img src={image} alt="cargando..." />
+    <div className={s.cargando}>
+      <img className={s.cargando_img} src={image} alt="cargando..." />
     </div>
   )
 }
