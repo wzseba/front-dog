7797913e25d6835cd01f27f48bb196343c7376a4
wzseba <wzseba@gmail.com>
2023-02-01 00:37:33 -0300

    function detail

--- src/redux/action.js
+++ src/redux/action.js
@@ -14,3 +14,19 @@ export function getAllDogs(){
         }
     }
 }
+
+export function getDetail(id){
+    return async function(dispatch){
+        try {
+            const res = await axios.get(`dogs/${id}`);
+            
+            dispatch({
+                type: "GET_DETAIL",
+                payload: res.data
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,7 +2,7 @@ const initialState = {
     dogs: [],
     temperaments: [],
     allDogs: [],
-    details: {}
+    detail: {}
 }
 
 const rootReducer = (state=initialState, action)=>{
@@ -14,6 +14,13 @@ const rootReducer = (state=initialState, action)=>{
                 // allDogs: action.payload
             }
         }
+        case "GET_DETAIL":{
+            return{
+                ...state,
+                detail: action.payload
+            }
+        }
+
         default: return state;
     }
 
