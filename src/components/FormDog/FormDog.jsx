63afaaa9318f2aa7e3140fe3bbab533000d8a056
wzseba <wzseba@gmail.com>
2023-02-05 23:34:28 -0300

    se arma estructura de formulario con validaciones

--- src/components/FormDog/FormDog.jsx
+++ src/components/FormDog/FormDog.jsx
@@ -1,77 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { getTemperaments } from '../../redux/action';
 
+const initialForm = {
+  name:'',
+  minHeight:'',
+  maxHeight:'',
+  minWeight:'',
+  maxWeight:'',
+  temperaments:[],
+  image:''
+}
+
+const validationsForm = (form)=>{
+  let errors = {};
+
+  /**corregir espacios en blanco */
+  const regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]{3,15}$/;
+  const regexUrl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&=]*)/
+  
+  if(form.name.includes(' ')){
+    errors.name = 'No puede contener espacios vacios'
+  }else if(!regexName.test(form.name.trim())){
+    errors.name = 'El campo solo acepta letras y un rango de 3 a 15 caracteres'
+  }
+  /**ALTURA */
+  if(!form.minHeight.trim()){
+    errors.minHeight = 'No puede ir vacio'
+  }else if(Number(form.minHeight) > Number(form.maxHeight)){
+    errors.minHeight = 'La altura minima no puede ser mayor a su altura maxima'
+  }
+
+  if(!form.maxHeight.trim()){
+    errors.maxHeight = 'No puede ir vacio'
+  }else if(Number(form.maxHeight) < Number(form.minHeight)){
+    errors.maxHeight = 'La altura maxima no puede ser menor a su altura minima'
+  }
+  /**PESO */
+  if(!form.minWeight.trim()){
+    errors.minWeight = 'No puede ir vacio'
+  }else if(Number(form.minWeight) > Number(form.maxWeight)){
+    errors.minWeight = 'El peso minimo no puede ser mayor a su peso maximo'
+  }
+
+  if(!form.maxWeight.trim()){
+    errors.maxWeight = 'No puede ir vacio'
+  }else if(Number(form.maxWeight) < Number(form.minWeight)){
+    errors.maxWeight = 'El peso maximo no puede ser menor a su peso minimo'
+  }
+
+  if(form.image.includes(' ')){
+    errors.image = 'No puede contener espacios vacios'
+  }else if(!regexUrl.test(form.image)){
+    errors.image = 'La url que ingreso no es un formato valido'
+  }
+
+  return errors;
+}
 
 const FormDog = () => {
 
+  const {
+    form,
+    errors,
+    handleChange,
+    handleBlur,
+    handleDelete,
+    handleSelect,
+    handleSubmit
+  } = useForm(initialForm,validationsForm)
+
     const dispatch = useDispatch();
     const allTemp = useSelector(state => state.temperaments);
-    const [error, setError] = useState({
-      name:'',
-      vacio:''
-    });
-    const [input, setInput] = useState({
-      name:'',
-      minHeight:'',
-      maxHeight:'',
-      minWeight:'',
-      maxWeight:'',
-      temperaments:[],
-      image:''
-    })
-
+   
     useEffect(() => {
       dispatch(getTemperaments());
-    
     }, [dispatch]);
-
-    /**Validacion de inputs */
-    const validate = ()=>{
-
-      let regName = /^[A-Za-z\u00f1\u00d1]{3,15}$/;
-  
-      if(regName.test(input.name)){
-        setError({...error, name:''});
-      }else{
-        setError({...error, name:'No puede estar vacio y debe ingresar de 4 a 14 caracteres alfabeticos'});
-      }
-
-    }
-
-    const handleChange = (e)=>{
-      
-      setInput({
-        ...input,
-        [e.target.name]: e.target.value
-      })
-      validate();
-    }
-
-    const handleBlur = (e)=>{
-      handleChange(e)
-    }
-    const handleSelect = (e)=>{
-      
-      setInput({
-        ...input,
-        temperaments:[...input.temperaments, e.target.value]
-      })
-      
-    }
-
-    const handleDelete = (e)=>{
-      setInput({
-        ...input,
-        temperaments: input.temperaments.filter( t => t !== e)
-      })
-      
-    }
-
-    const handleSubmit = (e)=>{
-      e.preventDefault();
-      console.log(input);
-    }
 
   return (
    <form onSubmit={handleSubmit}>
@@ -81,12 +86,13 @@ const FormDog = () => {
         <input 
         type="text" 
         name='name'
-        value={input.name}
+        value={form.name}
         placeholder='Nombre de Raza'
         onChange={handleChange}
-        onBlur={handleBlur}/>
+        onBlur={handleBlur}
+        required/>
       </label>
-      {error && <p>{error.name}</p>}
+    {errors.name && <p>{errors.name}</p>}
     </div>
 
     <div>
@@ -95,10 +101,15 @@ const FormDog = () => {
         <input 
         type="number" 
         name='maxHeight'
-        value={input.maxHeight}
+        min="4" 
+        max="140"
+        value={form.maxHeight}
         placeholder='Altura Maxima'
-        onChange={handleChange}/>
+        onChange={handleChange}
+        onBlur={handleBlur}
+        required/>
       </label>
+      {errors.maxHeight && <p>{errors.maxHeight}</p>}
     </div>
 
     <div>
@@ -107,10 +118,15 @@ const FormDog = () => {
         <input 
         type="number" 
         name='minHeight'
-        value={input.minHeight}
+        min="4" 
+        max="140"
+        value={form.minHeight}
         placeholder='Altura Minima'
-        onChange={handleChange}/>
+        onChange={handleChange}
+        onBlur={handleBlur}
+        required/>
       </label>
+      {errors.minHeight && <p>{errors.minHeight}</p>}
     </div>
 
     <div>
@@ -119,22 +135,31 @@ const FormDog = () => {
         <input 
         type="number" 
         name='maxWeight'
-        value={input.maxWeight}
+        min="4" 
+        max="90"
+        value={form.maxWeight}
         placeholder='Peso Maximo'
-        onChange={handleChange}/>
+        onChange={handleChange}
+        onBlur={handleBlur}
+        required/>
       </label>
     </div>
-
+    {errors.maxWeight && <p>{errors.maxWeight}</p>}
     <div>
       <label>
         Min Weight:
         <input 
         type="number" 
         name='minWeight'
-        value={input.minWeight}
+        min="4" 
+        max="90"
+        value={form.minWeight}
         placeholder='Peso Minimo'
-        onChange={handleChange}/>
+        onChange={handleChange}
+        onBlur={handleBlur}
+        required/>
       </label>
+      {errors.minWeight && <p>{errors.minWeight}</p>}
     </div>
 
     <div>
@@ -154,14 +179,17 @@ const FormDog = () => {
         <input 
         type="text" 
         name='image'
-        value={input.image}
+        value={form.image}
         placeholder='url'
-        onChange={handleChange}/>
+        onChange={handleChange}
+        onBlur={handleBlur}
+        />
       </label>
+      {errors.image && <p>{errors.image}</p>}
     </div>
     {/* mostrar por pantalla los temperamentos elegidos */}
     {
-      input.temperaments?.map((temp,index) => (
+      form.temperaments?.map((temp,index) => (
         <div key={index}>
           <p>{temp}</p>
           <button onClick={()=>handleDelete(temp)}>x</button>
@@ -169,7 +197,7 @@ const FormDog = () => {
       ))
     }
 
-    <button type='submit'>Crear Dog</button>
+    <input type='submit' value='Crear Dog'/>
    </form>
   )
 }
