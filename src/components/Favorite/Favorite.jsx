24aad9dacc029f5a8552f9385f6807d2f69b3890
wzseba <wzseba@gmail.com>
2023-02-13 23:42:45 -0300

    estructura y estilos responsive en proceso

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import { deleteFavorite } from '../../redux/action';
-import s from './Favorite.module.css'
+import { FaTrash } from 'react-icons/fa';
+import s from './Favorite.module.css';
+
 
 const Favorite = () => {
 
   const dispatch = useDispatch();
   const favorites = useSelector(state => state.favorites);
+  // const [open, setOpen] = useState(false);
 
   useEffect(()=>{
     localStorage.setItem("favorites", JSON.stringify(favorites));
   },[favorites]);
 
-  console.log('estoy en favoritos-->',favorites);
-
   const handleDeleteFavorite = (id)=>{
+    // setOpen(true);
     dispatch(deleteFavorite(id))  
-    console.log('este id',id);
   }
 
   return (
     <div className={s.container_favorito}>
-      <h2>Mis Perros Favoritos</h2>
-      <Link to='/home'>
+      <h2 className={s.fav_title}>Mis Perros Favoritos</h2>
+      <Link className={s.btn_volver} to='/home'>
         <button>Volver</button>
       </Link>
       
@@ -35,10 +36,10 @@ const Favorite = () => {
           
               <div key={fav.id} className={s.content_card_fav}>
               <img className={s.fav_img} src={fav.image} alt={fav.name}/>
-              <h3 className={s.fav_name}>Nombre: {fav.name}</h3>
-              <p className={s.fav_height}>Altura: {fav.height}</p>
-              <p className={s.fav_weight}>Peso: {fav.weight}</p>
-              <button onClick={()=>handleDeleteFavorite(fav.id)}>X</button>
+              <h3 className={s.fav_name}><span>Nombre:</span> {fav.name}</h3>
+              <p className={s.fav_height}><span>Altura:</span> {fav.height}</p>
+              <p className={s.fav_weight}><span>Peso:</span> {fav.weight}</p>
+              <button className={s.fav_btn_delete} onClick={()=>handleDeleteFavorite(fav.id)}><FaTrash/></button>
               </div>
       
               ))
