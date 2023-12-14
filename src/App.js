05b13f63d9b9a7be785640c368152a635ded21d7
wzseba <wzseba@gmail.com>
2023-12-14 20:22:20 -0300

    update: actualizacion deploy

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import Home from './components/Home/Home';
 import Detail from './components/Detail/Detail';
 import FormDog from './components/FormDog/FormDog';
 import Favorite from './components/Favorite/Favorite';
-
+import Register from './components/Register/Register';
 
 // axios.defaults.baseURL = 'http://localhost:3001/';
-// axios.defaults.baseURL = 'https://server-dogback.onrender.com/';
-axios.defaults.baseURL = 'https://dog-back-dev-dktb.3.us-1.fl0.io';
+axios.defaults.baseURL = 'https://server-dog.onrender.com/';
+// axios.defaults.baseURL = 'https://dog-back-dev-dktb.3.us-1.fl0.io';
 
 function App() {
   return (
@@ -26,6 +26,7 @@ function App() {
       <Route exact path="/favorite" component={Favorite} />
       <Route exact path="/detail/:id" component={Detail} />
       <Route exact path="/formAdd" component={FormDog} />
+      <Route exact path="/register" component={Register} />
       <Route path="/*" component={PageNotFound} /> 
       </Switch>
     </Router>
