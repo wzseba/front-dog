cdfc35d117d73cba3721d3579149616904144881
wzseba <wzseba@gmail.com>
2023-12-16 19:21:56 -0300

    refactor: reemplazo de tags por componente

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import s from './Landing.module.css';
-import { Link } from 'react-router-dom';
+
 import { useForm } from '../../hooks/useForm';
 import { validatorLogin } from '../../helpers/validatorForm';
 import { useDispatch } from 'react-redux';
 import { loginForm } from '../../redux/action';
+import FormLogin from '../FormLogin/FormLogin';
 
 const initialLogin = {
   email:'',
@@ -26,44 +27,12 @@ const Landing = () => {
         </div>
 
         <div className={s.de}>
-          <div>
-            <h1>DogLandia</h1>
-            <div className={s.link}>
-      
-              <form className={s.formulario} onSubmit={handleSubmit}>
-                <label className={s.container_label}>
-                  Email:
-                  <input 
-                  type="email"
-                  name='email'
-                  placeholder='Email'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required/>
-                </label>
-                {errors.email && <p className={s.errorParrafo}>{errors.email}</p>}
-
-                <label className={s.container_label}>
-                  Password:
-                  <input 
-                  type="password" 
-                  name='password'
-                  placeholder='Password'
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required/>
-                </label>
-                {errors.password && <p className={s.errorParrafo}>{errors.password}</p>}
-                <div className={s.submit}>
-                  <abbr title="Los usuarios pueden crear razas"><input type='submit' value='Iniciar Sesion'/></abbr>
-                  <abbr title="Los usuarios no pueden crear razas"><Link to='/home'>Invitado</Link></abbr>
-                </div>
-              </form>
-              <Link className={s.crearCuenta} to='/'>
-                Crear una cuenta
-              </Link>
-            </div>
-          </div>
+          <FormLogin
+            handleSubmit={handleSubmit}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+            errors={errors}
+          />
 
           <div className={s.sociales}>
             <ul>
