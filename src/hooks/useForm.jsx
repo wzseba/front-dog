3c3a7159d313faf07b91a1ca0a15eb7d56e0fa7d
wzseba <wzseba@gmail.com>
2024-02-06 22:30:18 -0300

    refactor: se borraron comentarios y espacios vacios

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,66 +1,62 @@
 import { useState } from "react";
 
+export const useForm = (initialForm, validateForm, dispatch, enviarData) => {
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
 
-export const useForm = (initialForm,validateForm,dispatch,enviarData)=>{
+  /**manejadores de eventos */
 
-   const [form,setForm] = useState(initialForm);
-   const [errors,setErrors] = useState({});
-
-   /**manejadores de eventos */
-
-   const handleChange = (e)=>{
-    const {name, value} = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
     setForm({
-        ...form,
-        [name]:value
-    })
-   }
+      ...form,
+      [name]: value,
+    });
+  };
 
-   const handleBlur = (e)=>{
+  const handleBlur = (e) => {
     handleChange(e);
     /** Setea el error de cada propiedad del objeto form*/
-    setErrors(validateForm(form))
-   }
+    setErrors(validateForm(form));
+  };
 
-   const handleSelect = (e)=>{
-    const {value} = e.target
+  const handleSelect = (e) => {
+    const { value } = e.target;
     setForm({
-            ...form,
-         temperaments:[...form.temperaments, value]
-        })
-   }
+      ...form,
+      temperaments: [...form.temperaments, value],
+    });
+  };
 
-   const handleDelete = (e)=>{
+  const handleDelete = (e) => {
     setForm({
-        ...form,
-        temperaments: form.temperaments.filter( t => t !== e)
-    })
-   }
+      ...form,
+      temperaments: form.temperaments.filter((t) => t !== e),
+    });
+  };
 
-   const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(e.target);
-    if(Object.keys(errors).length === 0){
-        dispatch(enviarData(form));
-        console.log('enviando form al backend ', form);
-        // alert('Perrito Creado con exito');
-        
-        /**Se formatea estado */
-        console.log("initialForm:", initialForm);
-        setForm(initialForm);
-        setErrors(validateForm(form));
-    }
-        return;
-   }
 
-   return {
+    if (Object.keys(errors).length === 0) {
+      dispatch(enviarData(form));
+
+      /**Se formatea estado */
+      setForm(initialForm);
+      setErrors(validateForm(form));
+    }
+    return;
+  };
+
+  return {
     form,
     errors,
     handleChange,
     handleBlur,
     handleDelete,
     handleSelect,
-    handleSubmit
-   }
-}
+    handleSubmit,
+  };
+};
